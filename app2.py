1c516a2f2fc488f8adbdcdcdef62887a1081ab2d
xiling <shengjian.sj@alibaba-inc.com>
2024-07-21 10:37:53 +0800

    fix

--- app2.py
+++ app2.py
@@ -47,6 +47,7 @@ def llm_response(message):
     # llm = LLM().init_model('ChatGPT', model_path= 'gpt-3.5-turbo',api_key='fk227660-AHXAgsfcCN7YPNICluZK0DinAGYDpiNS')
     # llm = LLM().init_model('VllmGPT', model_path= 'THUDM/chatglm3-6b')
     # llm = LLM().init_model('Qwen', model_path= 'Qwen/Qwen-1_8B-Chat')
+    print(f"api_key:{opt.api_key}")
     llm = LLM().init_model('QwenBaiLian', api_key=opt.api_key)
     response = llm.chat(message)
     print(response)
